6fc89ba2c27ca525bd4ea15989036f213218ebf5
NewDividedc <newdividedc@NewDividedc.DHCP>
2020-05-07 16:50:55 +0800

    test shellcheck

--- test.sh
+++ test.sh
@@ -4,6 +4,8 @@ for file in chap0x04/code/*.sh;do
 	if [[ $file =~ $0 ]];then
 		continue
 	fi
-	printf "================= %s ====================\n" "$file"
+	printf "==================== %s ====================\n" "$file"
 	bash "$file"
+	printf "-------------------- %s --------------------\n" "$file"
+	shellcheck "$file"
 done
